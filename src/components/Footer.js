4db2671646fec6a98f085c70507d0e2a611bcca7
bseton11-YJKnqQ <bseton11@gmail.com>
2022-03-10 18:52:21 -0600

    added footer and icons from ant design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { LinkedinFilled, GithubFilled, MailFilled } from '@ant-design/icons'
 
 const styles = {
     footer: {
-        justifyContent: 'space-evenly'
-    }
+        justifyContent: 'space-evenly',
+        fontSize: 100,
+    },
+    margin: {
+        margin: 'auto',
+    },
 }
 
 export default function Footer () {
     return (
-        <footer>
+        <div style={styles.margin}>
             <ul style={styles.footer} className="nav nav-tabs">
-                <li>Email</li>
-                <li>LinkedIn</li>
-                <li>Github</li>
+                <li><a href='mailto:blehjooo@gmail.com'><MailFilled /></a></li>
+                <li><a href='https://www.linkedin.com/in/bleh-s/'><LinkedinFilled /></a></li>
+                <li><a href='https://github.com/Blehjo'><GithubFilled /></a></li>
             </ul>
-        </footer>
+        </div>
     )
 }
