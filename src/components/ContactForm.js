6c0949f4aee6977b119a2ceee14c0b7b6123df1b
Blehjo <blehjooo@gmail.com>
2022-03-14 12:06:23 -0500

    updated error messages on contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import emailjs from '@emailjs/browser';
+import ValidateForm from '../hooks/validateForm';
 
 // const service = process.env.SERVICE_ID;
 // const template = process.env.TEMPLATE_ID;
@@ -41,21 +42,29 @@ export default function ContactUs() {
             e.target.reset()
     }
 
+    const formSubmit = () =>{
+        console.log("callback function when form is submitted!");
+    }
+    // custom hook call
+    const{ errors, handleChange } = ValidateForm(formSubmit)
+
     return(
         <div>
-            <img style={styles.inline} alt='profile picture' width='300px' height='auto' src={require('../assets/profilepicture/newprofilepic.jpg')}/>
+            <img style={styles.inline} alt='profile' width='300px' height='auto' src={require('../assets/profilepicture/newprofilepic.jpg')}/>
             <div style={styles.format}>
                 <div className='container'>
                     <form onSubmit={sendEmail}>
                         <div style={styles.div} className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder='Name' name="name"/>
-                            {/* {value ? <h3></h3> : <h3 className='red'>Please put in name</h3>} */}
+                            <input type="text" className="form-control" placeholder='Name' minLength='2' name="contactname" onChange={handleChange}/>
+                            {errors.contactname && <p className='red'>{errors.contactname}</p>}
                         </div>
                         <div style={styles.div} className='col-8 form-group pt-2 mx-auto'>
-                            <input type='email' className="form-control" placeholder="Email Address" name="email"/>
+                            <input type='email' className="form-control" placeholder="Email Address" name="email" onChange={handleChange}/>
+                            {errors.email && <p className='red'>{errors.email}</p>}
                         </div>
                         <div style={styles.div} className='col-8 form-group pt-2 mx-auto'>
-                            <input style={styles.div} className='form-control' placeholder='Your message' name='message'></input>
+                            <input style={styles.div} className='form-control' minLength='2' placeholder='Your message' name='message' onChange={handleChange}></input>
+                            {errors.message && <p className='red'>{errors.message}</p>}
                         </div>
                         <div>
                             <input style={styles.button} type="submit" className="btn btn-info" value="Send Message"/>
