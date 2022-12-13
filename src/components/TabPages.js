dce9fbc191e978d6f70fa20e7c13a8041f0c0fc9
Blehjo <blehjooo@gmail.com>
2022-12-12 20:05:25 -0600

    updated applications tab in tabpage component

--- src/components/TabPages.js
+++ src/components/TabPages.js
@@ -7,7 +7,7 @@ import TechStack from './TechStack';
 const TabPages = () => {
     return (
         <Tabs
-        defaultActiveKey="applications"
+        defaultActiveKey="featured"
         id="justify-tab-example"
         className="mb-3"
         justify
@@ -17,12 +17,12 @@ const TabPages = () => {
                     Innovative software engineer with a background in public health databases who brings a unique perspective to frameworks for problem solving. Excels in troubleshooting issues with a high level of creativity to get results. Effective at communication not only verbally but as an active listener. Also, utilizes an agile management style to optimize project management. In addition, uses time management to achieve goals and keep on task to hit deadlines.
                 </Card>
             </Tab>
-            <Tab eventKey="applications" title="Applications">
+            <Tab eventKey="featured" title="Featured Applications">
                 {Array.from(ApplicationData).map(({ id, applicationTitle, mainPhoto, description, applicationImage, applicationLink, github }) => (
                     <Card style={{ margin: '1rem' }}>
                         <Row lg={2} xl={2}>
                             <Col lg={4} xl={4}>
-                                {/* <Card.Img src={require(mainPhoto)}/> */}
+                            <Card.Img height='180' style={{ objectFit:'cover'}} src={`https://drive.google.com/uc?export=view&id=${mainPhoto}`} alt={applicationTitle}/>
                             </Col>
                             <Col lg={8} xl={8}>
                                 <Card.Body>
