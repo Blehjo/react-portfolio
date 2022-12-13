474e28c3fa363b7c5bcf1b9564ca32c2156d4847
bseton11-YJKnqQ <bseton11@gmail.com>
2022-12-13 03:26:54 -0600

    updated image height

--- src/components/TabPages.js
+++ src/components/TabPages.js
@@ -20,21 +20,21 @@ const TabPages = () => {
             <Tab eventKey="featured" title="Featured Applications">
                 {Array.from(ApplicationData).map(({ id, applicationTitle, mainPhoto, description, applicationImage, applicationLink, github }) => (
                     <Card key={id} style={{ height: '180', margin: '1rem' }}>
-                        <Row lg={2} xl={2}>
-                            <Col lg={4} xl={4}>
-                            <Card.Img height='210' style={{ objectFit:'cover'}} src={`https://drive.google.com/uc?export=view&id=${mainPhoto}`} alt={applicationTitle}/>
-                            </Col>
-                            <Col lg={8} xl={8}>
-                                <Card.Body>
-                                    <Card.Title>{applicationTitle}</Card.Title> 
-                                    <Card.Text>{description}</Card.Text>
-                                </Card.Body>
+                        <Row md={1} lg={2} xl={2}>
+                            <Col lg={6} xl={6}>
+                            <Card.Img height='200' style={{ objectFit:'cover'}} src={`https://drive.google.com/uc?export=view&id=${mainPhoto}`} alt={applicationTitle}/>
                                 <Card.Footer>
                                     <Nav style={{ justifyContent: 'space-evenly', fontSize: 35, }}>
                                         <Nav.Link href={applicationLink} target='_blank' rel="noreferrer"><GlobalOutlined/></Nav.Link>
                                         <Nav.Link href={github} target='_blank' rel="noreferrer"><GithubFilled/></Nav.Link>
                                     </Nav>
                                 </Card.Footer>
+                            </Col>
+                            <Col lg={6} xl={6}>
+                                <Card.Body>
+                                    <Card.Title>{applicationTitle}</Card.Title> 
+                                    <Card.Text>{description}</Card.Text>
+                                </Card.Body>
                             </Col>
                         </Row>
                     </Card>
