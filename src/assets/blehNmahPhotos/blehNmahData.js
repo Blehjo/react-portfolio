868998162f75eb9c07d02d02520bad8f546810e6
bseton11-YJKnqQ <bseton11@gmail.com>
2022-12-13 05:52:03 -0600

    added new application

---
+++ src/assets/blehNmahPhotos/blehNmahData.js
@@ -0,0 +1,16 @@
+const blehNmah1 = "1hxUTfCzTUHAyLlOh39B4nWfm0hhbj3vS";
+const blehNmah2 = "17Zg5NROQ6qLyoRnu9a05Xrfku1lR5V40";
+const blehNmah3 = "1O2A7_kcRMcx8VidIvvkfFH5zIVcZLUZr";
+const blehNmah4 = "1eI-X1dGf9dZtM3t-nVnOkysmIMTqtQBW";
+const blehNmah5 = "1qGfvhZWV7oevNuQrYaXG14xA9WONXfGq";
+const blehNmah6 = "1fIuUIEID7UlXAMzcRQfiE5No8QMxjbVJ";
+
+
+export const blehNmahArr = [
+    blehNmah1,
+    blehNmah2,
+    blehNmah3,
+    blehNmah4,
+    blehNmah5,
+    blehNmah6,
+];
