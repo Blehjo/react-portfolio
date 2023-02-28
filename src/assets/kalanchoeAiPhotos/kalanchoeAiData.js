673106e6c54667aa20300d994167f033a0ba9eae
bseton11-YJKnqQ <bseton11@gmail.com>
2023-02-28 10:53:23 -0600

    updated application information

---
+++ src/assets/kalanchoeAiPhotos/kalanchoeAiData.js
@@ -0,0 +1,19 @@
+const kalanchoeAi1 = "https://i.imgur.com/U8X1MME.png";
+const kalanchoeAi2 = "https://i.imgur.com/iLdnnuZ.png";
+const kalanchoeAi3 = "https://i.imgur.com/nrqDcfT.png";
+const kalanchoeAi4 = "https://i.imgur.com/uEfFyGL.png";
+const kalanchoeAi5 = "https://i.imgur.com/U3RzGdu.png";
+const kalanchoeAi6 = "https://i.imgur.com/oj36eVv.png";
+const kalanchoeAi7 = "https://i.imgur.com/PQgALzE.png";
+const kalanchoeAi8 = "https://i.imgur.com/cvQYmOU.png";
+
+export const kalanchoeAiArr = [
+    kalanchoeAi7,
+    kalanchoeAi1,
+    kalanchoeAi2,
+    kalanchoeAi3,
+    kalanchoeAi4,
+    kalanchoeAi5,
+    kalanchoeAi6,
+    kalanchoeAi8,
+];
